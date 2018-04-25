48e408c67afbd3752f1f6686dd696c7b4e4d7009
Ben Fersenheim <benferse@users.noreply.github.com>
2018-04-25 17:42:20 -0400

    Switch to solarized theme since material has issues in nvim

--- config/hyper/.hyper.js
+++ config/hyper/.hyper.js
@@ -131,7 +131,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-material-theme"],
+  plugins: ["hyper-solarized-dark"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
