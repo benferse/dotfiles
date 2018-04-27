22178268d5986659095b0a97b1e54a0fc81179f9
Ben Fersenheim <benferse@users.noreply.github.com>
2018-04-27 16:52:23 -0400

    Less budget custom prompt construction

--- config/clink/clink-benferse.lua
+++ config/clink/clink-benferse.lua
@@ -1,15 +1,18 @@
 function setup_custom_prompt()
 
-    local cwd = clink.get_cwd()
-    local username = os.getenv('USERNAME')
-    local computer = os.getenv('COMPUTERNAME')
-    local template = "{u}@{c} {d}\n# " 
+    local token_table = {}
 
-    local with_cwd = string.gsub(template, "{d}", cwd)
-    local with_user = string.gsub(with_cwd, "{u}", username)
-    local with_comp = string.gsub(with_user, "{c}", computer)
+    token_table["{c}"] = os.getenv("COMPUTERNAME")
+    token_table["{u}"] = os.getenv("USERNAME")
+    token_table["{d}"] = clink.get_cwd
 
-    clink.prompt.value = with_comp
+    local custom_prompt = "\n{u}@{c} {d}\n# "
+
+    for key, value in pairs(token_table) do
+        custom_prompt = string.gsub(custom_prompt, key, value)
+    end
+
+    clink.prompt.value = custom_prompt 
 end
 
 clink.prompt.register_filter(setup_custom_prompt, 1)
