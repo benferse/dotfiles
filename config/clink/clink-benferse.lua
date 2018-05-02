3f4aa9635e2d5bfe9cea794e0400301f5dc9b93e
Ben Fersenheim <ben@bu.edu>
2018-05-02 11:36:05 -0400

    Pretty colors

--- config/clink/clink-benferse.lua
+++ config/clink/clink-benferse.lua
@@ -12,7 +12,7 @@ function setup_custom_prompt()
     token_table["{d}"] = clink.get_cwd
     token_table["{p}"] = ps_char
 
-    local custom_prompt = "\n{u}@{c} {d}\n{p} "
+    local custom_prompt = "\n\x1b[1;39;40m{u}@{c} {d}\n\x1b[1;39;40m{p} \x1b[0m"
 
     for key, value in pairs(token_table) do
         custom_prompt = string.gsub(custom_prompt, key, value)
