9200887533265b400e56ac04c35ea7c56d0fe30a
Ben Fersenheim <ben@bu.edu>
2020-05-09 12:19:42 -0400

    Added vim-ps1 and profile updates

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -8,3 +8,17 @@ Import-Module PSReadLine
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+<#
+.SYNOPSIS
+Sets the shell's location to the root of the current git repository
+.DESCRIPTION
+If invoked from any subdirectory of a git repository, this will determine
+the root of the repository (i.e. the location of the .git folder) and cd
+there.
+#>
+function Invoke-GoToGitRoot {
+    Set-Location "$(git rev-parse --show-toplevel)"
+}
+
+Set-Alias -Name root -Value Invoke-GoToGitRoot
