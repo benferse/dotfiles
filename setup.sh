13ad43f6989a9f4d363697c8340521175afe6849
Ben Fersenheim <ben@bu.edu>
2020-09-11 11:51:55 -0400

    Renamed dotfiles in the config tree so they aren't hidden by default

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ ln -svf $ConfigRoot/starship/starship.toml ~/.config/starship.toml
 
 echo Linking settings for git...
 rm -vf ~/.gitconfig
-ln -svf $ConfigRoot/git/.gitconfig ~/.gitconfig
+ln -svf $ConfigRoot/git/gitconfig ~/.gitconfig
 
 echo Linking settings for tmux...
 [ ! -d ~/.tmux/plugins/tpm ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -22,14 +22,14 @@ rm -rvf ~/.tmux.conf
 ln -svf $ConfigRoot/tmux/tmux.conf ~/.tmux.conf
 
 echo Linking settings for bash...
-[ -f ~/.bashrc ] && mv -vf ~/.bashrc ~/.bashrc.orig
-[ -f ~/.profile ] && mv -vf ~/.profile ~/.profile.orig
+[ -f ~/.bashrc ]       && mv -vf ~/.bashrc ~/.bashrc.orig
+[ -f ~/.profile ]      && mv -vf ~/.profile ~/.profile.orig
 [ -f ~/.bash_profile ] && mv -vf ~/.bash_profile ~/.bash_profile.orig
-[ -f ~/.bash_logout ] && mv -vf ~/.bash_logout ~/.bash_logout.orig
+[ -f ~/.bash_logout ]  && mv -vf ~/.bash_logout ~/.bash_logout.orig
 
-ln -svf $ConfigRoot/bash/.bashrc ~/.bashrc
-ln -svf $ConfigRoot/bash/.profile ~/.profile
-ln -svf $ConfigRoot/bash/.bash_logout ~/.bash_logout
+ln -svf $ConfigRoot/bash/bashrc ~/.bashrc
+ln -svf $ConfigRoot/bash/profile ~/.profile
+ln -svf $ConfigRoot/bash/bash_logout ~/.bash_logout
 rm -vf ~/.bash_profile
 
 echo Linking settings for dircolors...
@@ -38,4 +38,4 @@ ln -svf $ConfigRoot/dircolors/nord.dircolors ~/.dircolors
 
 echo Linking settings for readline...
 rm -vf ~/.inputrc
-ln -svf $ConfigRoot/readline/.inputrc ~/.inputrc
+ln -svf $ConfigRoot/readline/inputrc ~/.inputrc
