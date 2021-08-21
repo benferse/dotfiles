47f0660ab2369daaca7c31c615b0b47ac6edac64
Ben Fersenheim <ben@bu.edu>
2021-08-21 15:40:33 -0400

    Activate bold/italic nord themes where possible

--- config/nvim/lua/theme.lua
+++ config/nvim/lua/theme.lua
@@ -3,11 +3,17 @@
 --
 
 local cmd = vim.cmd
+local g = vim.g
 local set = vim.opt
 
 local function setup()
     set.termguicolors = true
     set.background = 'dark'
+
+    g.nord_bold = 1
+    g.nord_italic = 1
+    g.nord_italic_comments = 1
+    g.nord_underline = 1
 
     cmd([[colorscheme nord]])
 end
