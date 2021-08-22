d5a31a619db63da2625892d4db5876a5904267b3
Ben Fersenheim <ben@bu.edu>
2021-08-22 16:59:17 -0400

    Add toggleterm for tmux-like behavior, even on windows <3

--- config/nvim/pack/benferse/opt/host-nvim/lua/features/terminal.lua
+++ config/nvim/pack/benferse/opt/host-nvim/lua/features/terminal.lua
@@ -4,6 +4,7 @@
 local function setup(args)
     vim.cmd([[
         packadd vim-tmux-navigator
+        packadd toggleterm.nvim
     ]])
 
     vim.g.tmux_navigator_save_on_switch = 1
@@ -11,6 +12,24 @@ local function setup(args)
     if vim.fn.has('win32') == 0 then
         vim.g.tmux_navigator_no_mappings = 1
     end
+
+    require('toggleterm').setup {
+        open_mapping = [[<leader>`]],
+        hide_numbers = true,
+        shade_terminals = true,
+        start_in_insert = true,
+        insert_mappings = false,
+        persist_size = true,
+        direction = 'vertical',
+        size = function(term)
+            if term.direction == 'horizontal' then
+                return 15
+            elseif term.direction == 'vertical' then
+                return vim.o.columns * 0.4
+            end
+        end,
+        close_on_exit = true,
+    }
 end
 
 return { setup = setup }
