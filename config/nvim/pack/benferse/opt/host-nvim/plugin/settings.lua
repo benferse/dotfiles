1cacbfb2897da2ea063d8e9cd10db9f4e34c86b0
Ben Fersenheim <ben@bu.edu>
2021-09-06 15:55:39 -0400

    Fix relative numbering settings on buffer types that don't want numbers at all (e.g. dashboard)

--- config/nvim/pack/benferse/opt/host-nvim/plugin/settings.lua
+++ config/nvim/pack/benferse/opt/host-nvim/plugin/settings.lua
@@ -2,13 +2,15 @@
 -- the top level init.lua since they are host specific
 local augroup = require('utils').augroup
 
--- Auto switching between absolute and relative line numbering
+-- Auto switching between absolute and relative line numbering,
+-- but only if the buffer hasn't explicitly turned off
+-- numbering
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 local numbering = {
-    [[InsertEnter * set norelativenumber]],
-    [[InsertLeave * set relativenumber]]
+    [[InsertEnter * if &l:number | set norelativenumber | end]],
+    [[InsertLeave * if &l:number | set relativenumber | end]]
 }
 
 augroup('benferse#numbering', numbering, true)
