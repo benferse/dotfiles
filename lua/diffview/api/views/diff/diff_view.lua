6d62643dcbbb3673a79e9d264308329d9d2acff6
Ben Fersenheim <ben@bu.edu>
2021-11-07 12:13:12 -0500

    Squashed 'config/nvim/pack/benferse/opt/diffview.nvim/' content from commit d4ee04c
    git-subtree-dir: config/nvim/pack/benferse/opt/diffview.nvim
    git-subtree-split: d4ee04c9eb8951dcf1aa83d7a896bededfc68228

---
+++ lua/diffview/api/views/diff/diff_view.lua
@@ -0,0 +1,113 @@
+local oop = require("diffview.oop")
+local utils = require("diffview.utils")
+local EventEmitter = require("diffview.events").EventEmitter
+local DiffView = require("diffview.views.diff.diff_view").DiffView
+local CFileEntry = require("diffview.api.views.file_entry").CFileEntry
+local FilePanel = require("diffview.views.diff.file_panel").FilePanel
+local FileDict = require("diffview.git.file_dict").FileDict
+local Rev = require("diffview.git.rev").Rev
+local RevType = require("diffview.git.rev").RevType
+local git = require("diffview.git.utils")
+
+local M = {}
+
+---@class FileData
+---@field path string Path relative to git root.
+---@field oldpath string|nil If the file has been renamed, this should be the old path, oterhwise nil.
+---@field status string Git status symbol.
+---@field stats GitStats|nil
+---@field left_null boolean Indicates that the left buffer should be represented by the null buffer.
+---@field right_null boolean Indicates that the right buffer should be represented by the null buffer.
+---@field selected boolean|nil Indicates that this should be the initially selected file.
+
+---@class CDiffView
+---@field files any
+---@field fetch_files function A function that should return an updated list of files.
+---@field get_file_data function A function that is called with parameters `path: string` and `split: string`, and should return a list of lines that should make up the buffer.
+local CDiffView = DiffView
+CDiffView = oop.create_class("CDiffView", DiffView)
+
+---CDiffView constructor.
+---@param opt any
+function CDiffView:init(opt)
+  self.emitter = EventEmitter()
+  self.layout_mode = CDiffView.get_layout_mode()
+  self.nulled = false
+  self.ready = false
+  self.winopts = { left = {}, right = {} }
+  self.git_root = opt.git_root
+  self.git_dir = git.git_dir(opt.git_root)
+  self.rev_arg = opt.rev_arg
+  self.path_args = opt.path_args
+  self.left = opt.left
+  self.right = opt.right
+  self.options = opt.options
+  self.files = FileDict()
+  self.fetch_files = opt.update_files
+  self.get_file_data = opt.get_file_data
+  self.panel = FilePanel(
+    self.git_root,
+    self.files,
+    self.path_args,
+    self.rev_arg or git.rev_to_pretty_string(self.left, self.right)
+  )
+
+  local files = self:create_file_entries(opt.files)
+
+  for kind, entries in pairs(files) do
+    for _, entry in ipairs(entries) do
+      table.insert(self.files[kind], entry)
+    end
+  end
+  self.files:update_file_trees()
+end
+
+---@Override
+function CDiffView:get_updated_files()
+  return self:create_file_entries(self.fetch_files(self))
+end
+
+function CDiffView:create_file_entries(files)
+  local entries = {}
+
+  local sections = {
+    { kind = "working", files = files.working, left = self.left, right = self.right },
+    {
+      kind = "staged",
+      files = files.staged,
+      left = git.head_rev(self.git_root),
+      right = Rev(RevType.INDEX),
+    },
+  }
+
+  for _, v in ipairs(sections) do
+    entries[v.kind] = {}
+    for _, file_data in ipairs(v.files) do
+      table.insert(
+        entries[v.kind],
+        CFileEntry({
+          path = file_data.path,
+          oldpath = file_data.oldpath,
+          absolute_path = utils.path_join({ self.git_root, file_data.path }),
+          status = file_data.status,
+          stats = file_data.stats,
+          kind = v.kind,
+          left = v.left,
+          right = v.right,
+          left_null = file_data.left_null,
+          right_null = file_data.right_null,
+          get_file_data = self.get_file_data,
+        })
+      )
+
+      if file_data.selected == true then
+        self.panel.cur_file = entries[v.kind][#entries[v.kind]]
+      end
+    end
+  end
+
+  return entries
+end
+
+M.CDiffView = CDiffView
+return M
