5bd75baeedaf2261d5a56c81804594e7f25ee26b
Ben Fersenheim <ben@bu.edu>
2021-11-07 14:57:01 -0500

    Config for vsnip

--- config/nvim/pack/benferse/opt/host-nvim/lua/features/picker.lua
+++ config/nvim/pack/benferse/opt/host-nvim/lua/features/picker.lua
@@ -3,13 +3,27 @@
 
 local map = require('utils').map
 
+local function has_words_before()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local function feedkey(key, mode)
+    local x = vim.api.nvim_replace_termcodes(key, true, true, true)
+    vim.api.nvim_feedkeys(x, mode, true)
+end
+
 local function setup()
     vim.cmd([[
         packadd nvim-cmp
         packadd cmp-nvim-lsp
+        packadd vim-vsnip
+        packadd cmp-vsnip
         packadd telescope.nvim
         packadd nvim-tree.lua
     ]])
+
+    -- Telescope for fuzzy finding all the things
 
     local telescope = require('telescope')
     local actions = require('telescope.actions')
@@ -34,15 +48,7 @@ local function setup()
 
     telescope.load_extension('projects')
 
-    -- Cool, thanks @allizon!
-    local function find_nvim_config()
-        require("telescope.builtin").find_files {
-            cwd = "~/.config/nvim",
-            prompt_title = "nvim config",
-        }
-    end
-
-    map('n', '<leader>vf', '<cmd>lua find_nvim_config()<cr>')
+    -- nvim-tree, because sometimes you just want a file explorer
 
     local tree = require('nvim-tree');
     tree.setup {
@@ -65,9 +71,46 @@ local function setup()
     cmp.setup {
         sources = cmp.config.sources {
             { name = 'nvim_lsp' },
+            { name = 'vsnip' },
+        },
+        snippet = {
+            expand = function(args)
+                vim.fn["vsnip#anonymous"](args.body)
+            end,
         },
         mapping = {
-            ['<cr>'] = cmp.mapping.confirm({ select = true }),
+            ['<cr>'] = cmp.mapping({
+                i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true, }),
+                c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+            }),
+            ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+            ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif vim.fn["vsnip#available"](1) == 1 then
+                    feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                elseif has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                    feedkey("<Plug>(vsnip-jump-prev)", "")
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
         }
     }
 end
