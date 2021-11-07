1d16f25d23084004ea28835fe0d25ed50a32bfd3
Ben Fersenheim <ben@bu.edu>
2021-11-07 12:27:54 -0500

    Squashed 'config/nvim/pack/benferse/opt/which-key.nvim/' content from commit d3032b6d
    git-subtree-dir: config/nvim/pack/benferse/opt/which-key.nvim
    git-subtree-split: d3032b6d3e0adb667975170f626cb693bfc66baa

---
+++ lua/which-key/keys.lua
@@ -0,0 +1,565 @@
+local Tree = require("which-key.tree")
+local Util = require("which-key.util")
+local Config = require("which-key.config")
+
+-- secret character that will be used to create <nop> mappings
+local secret = "Þ"
+
+---@class Keys
+local M = {}
+
+M.functions = {}
+M.operators = {}
+M.nowait = {}
+M.blacklist = {}
+
+function M.setup()
+  local builtin_ops = require("which-key.plugins.presets").operators
+  for op, _ in pairs(builtin_ops) do
+    M.operators[op] = true
+  end
+  local mappings = {}
+  for op, label in pairs(Config.options.operators) do
+    M.operators[op] = true
+    if builtin_ops[op] then
+      mappings[op] = { name = label, i = { name = "inside" }, a = { name = "around" } }
+    end
+  end
+  for _, t in pairs(Config.options.triggers_nowait) do
+    M.nowait[t] = true
+  end
+  M.register(mappings, { mode = "n", preset = true })
+  M.register({ i = { name = "inside" }, a = { name = "around" } }, { mode = "v", preset = true })
+  for mode, blacklist in pairs(Config.options.triggers_blacklist) do
+    for _, prefix in ipairs(blacklist) do
+      M.blacklist[mode] = M.blacklist[mode] or {}
+      M.blacklist[mode][prefix] = true
+    end
+  end
+end
+
+function M.get_operator(prefix)
+  for op, _ in pairs(Config.options.operators) do
+    if prefix:sub(1, #op) == op then
+      return op
+    end
+  end
+end
+
+function M.process_motions(ret, mode, prefix, buf)
+  local operator = mode == "v" and "" or M.get_operator(prefix)
+  if (mode == "n" or mode == "v") and operator then
+    local op_prefix = prefix:sub(#operator + 1)
+    local op_count = op_prefix:match("^(%d+)")
+    if op_count == "0" then
+      op_count = nil
+    end
+    if Config.options.motions.count == false then
+      op_count = nil
+    end
+    if op_count then
+      op_prefix = op_prefix:sub(#op_count + 1)
+    end
+    local op_results = M.get_mappings("o", op_prefix, buf)
+
+    if not ret.mapping and op_results.mapping then
+      ret.mapping = op_results.mapping
+      ret.mapping.prefix = prefix
+      ret.keys = Util.parse_keys(ret.prefix)
+    end
+
+    for _, mapping in pairs(op_results.mappings) do
+      mapping.prefix = operator .. (op_count or "") .. mapping.prefix
+      mapping.keys = Util.parse_keys(mapping.prefix)
+      table.insert(ret.mappings, mapping)
+    end
+  end
+end
+
+---@return MappingGroup
+function M.get_mappings(mode, prefix, buf)
+  ---@class MappingGroup
+  ---@field mode string
+  ---@field prefix string
+  ---@field buf number
+  ---@field mapping Mapping
+  ---@field mappings VisualMapping[]
+  local ret
+  ret = { mapping = nil, mappings = {}, mode = mode, buf = buf, prefix = prefix }
+
+  local prefix_len = #Util.parse_keys(prefix).nvim
+
+  ---@param node Node
+  local function add(node)
+    if node then
+      if node.mapping then
+        ret.mapping = vim.tbl_deep_extend("force", {}, ret.mapping or {}, node.mapping)
+      end
+      for k, child in pairs(node.children) do
+        if child.mapping and child.mapping.label ~= "which_key_ignore" then
+          ret.mappings[k] = vim.tbl_deep_extend("force", {}, ret.mappings[k] or {}, child.mapping)
+        end
+      end
+    end
+  end
+
+  local plugin_context = { buf = buf, mode = mode }
+  add(M.get_tree(mode).tree:get(prefix, nil, plugin_context))
+  add(M.get_tree(mode, buf).tree:get(prefix, nil, plugin_context))
+
+  -- Handle motions
+  M.process_motions(ret, mode, prefix, buf)
+
+  -- Fix labels
+  local tmp = {}
+  for _, value in pairs(ret.mappings) do
+    value.key = value.keys.nvim[prefix_len + 1]
+    value.key = vim.fn.strtrans(value.key)
+    if Config.options.key_labels[value.key] then
+      value.key = Config.options.key_labels[value.key]
+    end
+    local skip = not value.label and Config.options.ignore_missing == true
+    if Util.t(value.key) == Util.t("<esc>") then
+      skip = true
+    end
+    if not skip then
+      if value.group then
+        value.label = value.label or "+prefix"
+        value.label = value.label:gsub("^%+", "")
+        value.label = Config.options.icons.group .. value.label
+      elseif not value.label then
+        value.label = value.cmd or ""
+        for _, v in ipairs(Config.options.hidden) do
+          value.label = value.label:gsub(v, "")
+        end
+      end
+      if value.value then
+        value.value = vim.fn.strtrans(value.value)
+      end
+      table.insert(tmp, value)
+    end
+  end
+
+  -- Sort items, but not for plugins
+  table.sort(tmp, function(a, b)
+    if a.order and b.order then
+      return a.order < b.order
+    end
+    if a.group == b.group then
+      local ak = (a.key or ""):lower()
+      local bk = (b.key or ""):lower()
+      local aw = ak:match("[a-z]") and 1 or 0
+      local bw = bk:match("[a-z]") and 1 or 0
+      if aw == bw then
+        return ak < bk
+      end
+      return aw < bw
+    else
+      return (a.group and 1 or 0) < (b.group and 1 or 0)
+    end
+  end)
+  ret.mappings = tmp
+
+  return ret
+end
+
+---@param mappings Mapping[]
+---@return Mapping[]
+function M.parse_mappings(mappings, value, prefix)
+  prefix = prefix or ""
+  if type(value) == "string" then
+    table.insert(mappings, { prefix = prefix, label = value })
+  elseif type(value) == "table" then
+    if #value == 0 then
+      -- key group
+      for k, v in pairs(value) do
+        if k ~= "name" then
+          M.parse_mappings(mappings, v, prefix .. k)
+        end
+      end
+      if prefix ~= "" then
+        if value.name then
+          value.name = value.name:gsub("^%+", "")
+        end
+        table.insert(mappings, { prefix = prefix, label = value.name, group = true })
+      end
+    else
+      -- key mapping
+      ---@type Mapping
+      local mapping
+      mapping = { prefix = prefix, opts = {}, buf = M.get_buf_option(value) }
+      for k, v in pairs(value) do
+        if k == 1 then
+          mapping.label = v
+        elseif k == 2 then
+          mapping.cmd = mapping.label
+          mapping.label = v
+        elseif k == "noremap" then
+          mapping.opts.noremap = v
+        elseif k == "silent" then
+          mapping.opts.silent = v
+        elseif k == "mode" then
+          mapping.mode = v
+        elseif k == "expr" then
+          mapping.opts.expr = v
+        elseif k == "plugin" then
+          mapping.group = true
+          mapping.plugin = v
+        else
+          error("Invalid key mapping: " .. vim.inspect(value))
+        end
+      end
+      if mapping.cmd and type(mapping.cmd) == "function" then
+        table.insert(M.functions, mapping.cmd)
+        if mapping.opts.expr then
+          mapping.cmd = string.format([[luaeval('require("which-key").execute(%d)')]], #M.functions)
+        else
+          mapping.cmd = string.format([[<cmd>lua require("which-key").execute(%d)<cr>]], #M.functions)
+        end
+      end
+      table.insert(mappings, mapping)
+    end
+  else
+    error("Invalid mapping " .. vim.inspect(value))
+  end
+  return mappings
+end
+
+function M.get_buf_option(opts)
+  for _, k in pairs({ "buffer", "bufnr", "buf" }) do
+    if opts[k] then
+      local v = opts[k]
+      if v == 0 then
+        v = vim.api.nvim_get_current_buf()
+      end
+      opts[k] = nil
+      if k == "buffer" then
+        return v
+      elseif k == "bufnr" or k == "buf" then
+        Util.warn(string.format([[please use "buffer" instead of %q for buffer mappings]], k))
+        return v
+      end
+    end
+  end
+end
+
+---@type table<string, MappingTree>
+M.mappings = {}
+M.duplicates = {}
+
+function M.map(mode, prefix, cmd, buf, opts)
+  local other = vim.api.nvim_buf_call(buf, function()
+    local ret = vim.fn.maparg(prefix, mode, false, true)
+    ---@diagnostic disable-next-line: undefined-field
+    return (ret and ret.lhs and ret.rhs ~= cmd) and ret or nil
+  end)
+  if other then
+    table.insert(M.duplicates, { mode = mode, prefix = prefix, cmd = cmd, buf = buf, other = other })
+  end
+  cmd = cmd:gsub("[\\]", "<bslash>")
+  if buf ~= nil then
+    pcall(vim.api.nvim_buf_set_keymap, buf, mode, prefix, cmd, opts)
+  else
+    pcall(vim.api.nvim_set_keymap, mode, prefix, cmd, opts)
+  end
+end
+
+function M.register(mappings, opts)
+  opts = opts or {}
+
+  local prefix = opts.prefix or ""
+  local mode = opts.mode or "n"
+
+  opts.buffer = M.get_buf_option(opts)
+
+  mappings = M.parse_mappings({}, mappings, prefix)
+
+  -- always create the root node for the mode, even if there's no mappings,
+  -- to ensure we have at least a trigger hooked for non documented keymaps
+  M.get_tree(mode)
+
+  for _, mapping in pairs(mappings) do
+    if opts.buffer and not mapping.buf then
+      mapping.buf = opts.buffer
+    end
+    if opts.preset then
+      mapping.preset = true
+    end
+    mapping.keys = Util.parse_keys(mapping.prefix)
+    mapping.mode = mapping.mode or mode
+    if mapping.cmd then
+      mapping.opts = vim.tbl_deep_extend("force", { silent = true, noremap = true }, opts, mapping.opts or {})
+      local keymap_opts = {
+        silent = mapping.opts.silent,
+        noremap = mapping.opts.noremap,
+        nowait = mapping.opts.nowait or false,
+        expr = mapping.opts.expr or false,
+      }
+      if mapping.cmd:lower():sub(1, #"<plug>") == "<plug>" then
+        keymap_opts.noremap = false
+      end
+      M.map(mapping.mode, mapping.prefix, mapping.cmd, mapping.buf, keymap_opts)
+    end
+    M.get_tree(mapping.mode, mapping.buf).tree:add(mapping)
+  end
+end
+
+M.hooked = {}
+
+function M.hook_id(prefix, mode, buf)
+  return mode .. (buf or "") .. Util.t(prefix)
+end
+
+function M.is_hooked(prefix, mode, buf)
+  return M.hooked[M.hook_id(prefix, mode, buf)]
+end
+
+function M.hook_del(prefix, mode, buf)
+  local id = M.hook_id(prefix, mode, buf)
+  M.hooked[id] = nil
+  if buf then
+    pcall(vim.api.nvim_buf_del_keymap, buf, mode, prefix)
+    pcall(vim.api.nvim_buf_del_keymap, buf, mode, prefix .. secret)
+  else
+    pcall(vim.api.nvim_del_keymap, mode, prefix)
+    pcall(vim.api.nvim_del_keymap, mode, prefix .. secret)
+  end
+end
+
+function M.hook_add(prefix, mode, buf, secret_only)
+  -- check if this trigger is blacklisted
+  if M.blacklist[mode] and M.blacklist[mode][prefix] then
+    return
+  end
+  -- don't hook numbers. See #118
+  if tonumber(prefix) then
+    return
+  end
+  -- don't hook to j or k in INSERT mode
+  if mode == "i" and (prefix == "j" or prefix == "k") then
+    return
+  end
+  -- never hook q
+  if mode == "n" and prefix == "q" then
+    return
+  end
+  -- never hook into select mode
+  if mode == "s" then
+    return
+  end
+  -- never hook into operator pending mode
+  -- this is handled differently
+  if mode == "o" then
+    return
+  end
+  if prefix == Util.t("<esc>") then
+    return
+  end
+  -- never hook into operators in visual mode
+  if (mode == "v" or mode == "x") and M.operators[prefix] then
+    return
+  end
+
+  -- Check if we need to create the hook
+  if type(Config.options.triggers) == "string" and Config.options.triggers ~= "auto" then
+    if Util.t(prefix) ~= Util.t(Config.options.triggers) then
+      return
+    end
+  end
+  if type(Config.options.triggers) == "table" then
+    local ok = false
+    for _, trigger in pairs(Config.options.triggers) do
+      if Util.t(trigger) == Util.t(prefix) then
+        ok = true
+        break
+      end
+    end
+    if not ok then
+      return
+    end
+  end
+
+  local opts = { noremap = true, silent = true }
+  local id = M.hook_id(prefix, mode, buf)
+  local id_global = M.hook_id(prefix, mode)
+  -- hook up if needed
+  if not M.hooked[id] and not M.hooked[id_global] then
+    local cmd = [[<cmd>lua require("which-key").show(%q, {mode = %q, auto = true})<cr>]]
+    if vim.g.mapleader == "\\" or vim.g.mapleader == nil then
+      prefix = prefix:gsub("<[lL]eader>", "\\")
+    end
+    if vim.g.maplocalleader == "\\" or vim.g.maplocalleader == nil then
+      prefix = prefix:gsub("<[lL]ocalleader>", "\\")
+    end
+    cmd = string.format(cmd, prefix, mode)
+    -- map group triggers and nops
+    -- nops are needed, so that WhichKey always respects timeoutlen
+
+    local mapmode = mode == "v" and "x" or mode
+    if secret_only ~= true then
+      M.map(mapmode, prefix, cmd, buf, opts)
+    end
+    if not M.nowait[prefix] then
+      M.map(mapmode, prefix .. secret, "<nop>", buf, opts)
+    end
+
+    M.hooked[id] = true
+  end
+end
+
+function M.update(buf)
+  for k, tree in pairs(M.mappings) do
+    if tree.buf and not vim.api.nvim_buf_is_valid(tree.buf) then
+      -- remove group for invalid buffers
+      M.mappings[k] = nil
+    elseif not buf or not tree.buf or buf == tree.buf then
+      -- only update buffer maps, if:
+      -- 1. we dont pass a buffer
+      -- 2. this is a global node
+      -- 3. this is a local buffer node for the passed buffer
+      M.update_keymaps(tree.mode, tree.buf)
+      M.add_hooks(tree.mode, tree.buf, tree.tree.root)
+    end
+  end
+end
+
+---@param node Node
+function M.add_hooks(mode, buf, node, secret_only)
+  if not node.mapping then
+    node.mapping = { prefix = node.prefix, group = true, keys = Util.parse_keys(node.prefix) }
+  end
+  if node.prefix ~= "" and node.mapping.group == true and not node.mapping.cmd then
+    -- first non-cmd level, so create hook and make all decendents secret only
+    M.hook_add(node.prefix, mode, buf, secret_only)
+    secret_only = true
+  end
+  for _, child in pairs(node.children) do
+    M.add_hooks(mode, buf, child, secret_only)
+  end
+end
+
+function M.dump()
+  local ok = {}
+  local todo = {}
+  for _, tree in pairs(M.mappings) do
+    M.update_keymaps(tree.mode, tree.buf)
+    tree.tree:walk( ---@param node Node
+      function(node)
+        if node.mapping then
+          if node.mapping.label then
+            ok[node.mapping.prefix] = true
+            todo[node.mapping.prefix] = nil
+          elseif not ok[node.mapping.prefix] then
+            todo[node.mapping.prefix] = { node.mapping.cmd or "" }
+          end
+        end
+      end
+    )
+  end
+  return todo
+end
+
+function M.check_health()
+  vim.fn["health#report_start"]("WhichKey: checking conflicting keymaps")
+  for _, tree in pairs(M.mappings) do
+    M.update_keymaps(tree.mode, tree.buf)
+    tree.tree:walk( ---@param node Node
+      function(node)
+        local count = 0
+        for _ in pairs(node.children) do
+          count = count + 1
+        end
+
+        local auto_prefix = not node.mapping or (node.mapping.group == true and not node.mapping.cmd)
+        if node.prefix ~= "" and count > 0 and not auto_prefix then
+          local msg = ("conflicting keymap exists for mode **%q**, lhs: **%q**"):format(tree.mode, node.mapping.prefix)
+          vim.fn["health#report_warn"](msg)
+          local cmd = node.mapping.cmd or " "
+          vim.fn["health#report_info"](("rhs: `%s`"):format(cmd))
+        end
+      end
+    )
+  end
+  for _, dup in pairs(M.duplicates) do
+    local msg = ""
+    if dup.buf == dup.other.buffer then
+      msg = "duplicate keymap"
+    else
+      msg = "buffer-local keymap overriding global"
+    end
+    msg = (msg .. " for mode **%q**, buf: %d, lhs: **%q**"):format(dup.mode, dup.buf or 0, dup.prefix)
+    if dup.buf == dup.other.buffer then
+      vim.fn["health#report_error"](msg)
+    else
+      vim.fn["health#report_warn"](msg)
+    end
+    vim.fn["health#report_info"](("old rhs: `%s`"):format(dup.other.rhs or ""))
+    vim.fn["health#report_info"](("new rhs: `%s`"):format(dup.cmd or ""))
+  end
+end
+
+function M.get_tree(mode, buf)
+  if mode == "s" or mode == "x" then
+    mode = "v"
+  end
+  Util.check_mode(mode, buf)
+  local idx = mode .. (buf or "")
+  if not M.mappings[idx] then
+    M.mappings[idx] = { mode = mode, buf = buf, tree = Tree:new() }
+  end
+  return M.mappings[idx]
+end
+
+function M.is_hook(prefix, cmd)
+  -- skip mappings with our secret nop command
+  local has_secret = prefix:find(secret)
+  -- skip auto which-key mappings
+  local has_wk = cmd and cmd:find("which%-key") and cmd:find("auto") or false
+  return has_wk or has_secret
+end
+
+---@param mode string
+---@param buf number
+function M.update_keymaps(mode, buf)
+  ---@type Keymap
+  local keymaps = buf and vim.api.nvim_buf_get_keymap(buf, mode) or vim.api.nvim_get_keymap(mode)
+  local tree = M.get_tree(mode, buf).tree
+  for _, keymap in pairs(keymaps) do
+    local skip = M.is_hook(keymap.lhs, keymap.rhs)
+
+    if not skip and Util.t(keymap.rhs) == "" then
+      skip = true
+    end
+
+    -- check if <leader> was remapped
+    if not skip and Util.t(keymap.lhs) == Util.t("<leader>") and mode == "n" then
+      if Util.t(keymap.rhs) == "" then
+        skip = true
+      else
+        Util.warn(
+          string.format(
+            "Your <leader> key for %q mode in buf %d is currently mapped to %q. WhichKey automatically creates triggers, so please remove the mapping",
+            mode,
+            buf or 0,
+            keymap.rhs
+          )
+        )
+      end
+    end
+
+    if not skip then
+      local mapping = {
+        id = Util.t(keymap.lhs),
+        prefix = keymap.lhs,
+        cmd = keymap.rhs,
+        keys = Util.parse_keys(keymap.lhs),
+      }
+      -- don't include Plug keymaps
+      if mapping.keys.nvim[1]:lower() ~= "<plug>" then
+        tree:add(mapping)
+      end
+    end
+  end
+end
+
+return M
---
+++ lua/which-key/layout.lua
@@ -0,0 +1,203 @@
+local Config = require("which-key.config")
+local Text = require("which-key.text")
+local Keys = require("which-key.keys")
+local Util = require("which-key.util")
+
+---@class Layout
+---@field mapping Mapping
+---@field items VisualMapping[]
+---@field options Options
+---@field text Text
+---@field results MappingGroup
+local Layout = {}
+Layout.__index = Layout
+
+---@param mappings MappingGroup
+---@param options Options
+function Layout:new(mappings, options)
+  options = options or Config.options
+  local this = {
+    results = mappings,
+    mapping = mappings.mapping,
+    items = mappings.mappings,
+    options = options,
+    text = Text:new(),
+  }
+  setmetatable(this, self)
+  return this
+end
+
+function Layout:max_width(key)
+  local max = 0
+  for _, item in pairs(self.items) do
+    if item[key] and Text.len(item[key]) > max then
+      max = Text.len(item[key])
+    end
+  end
+  return max
+end
+
+function Layout:trail()
+  local prefix = self.results.prefix
+  local buf_path = Keys.get_tree(self.results.mode, self.results.buf).tree:path(prefix)
+  local path = Keys.get_tree(self.results.mode).tree:path(prefix)
+  local len = #self.results.mapping.keys.nvim
+  local cmd_line = { { " " } }
+  for i = 1, len, 1 do
+    local node = buf_path[i]
+    if not (node and node.mapping and node.mapping.label) then
+      node = path[i]
+    end
+    local step = self.mapping.keys.nvim[i]
+    if node and node.mapping and node.mapping.label then
+      step = self.options.icons.group .. node.mapping.label
+    end
+    if Config.options.key_labels[step] then
+      step = Config.options.key_labels[step]
+    end
+    table.insert(cmd_line, { step, "WhichKeyGroup" })
+    if i ~= #self.mapping.keys.nvim then
+      table.insert(cmd_line, { " " .. self.options.icons.breadcrumb .. " ", "WhichKeySeparator" })
+    end
+  end
+  local width = 0
+  for _, line in pairs(cmd_line) do
+    width = width + Text.len(line[1])
+  end
+  local help = { --
+    ["<bs>"] = "go up one level",
+    ["<esc>"] = "close",
+  }
+  if #self.text.lines > self.options.layout.height.max then
+    help[Config.options.popup_mappings.scroll_down] = "scroll down"
+    help[Config.options.popup_mappings.scroll_up] = "scroll up"
+  end
+  local help_line = {}
+  local help_width = 0
+  for key, label in pairs(help) do
+    help_width = help_width + Text.len(key) + Text.len(label) + 2
+    table.insert(help_line, { key .. " ", "WhichKey" })
+    table.insert(help_line, { label .. " ", "WhichKeySeparator" })
+  end
+  table.insert(cmd_line, { string.rep(" ", math.floor(vim.o.columns / 2 - help_width / 2) - width) })
+
+  if self.options.show_help then
+    for _, l in pairs(help_line) do
+      table.insert(cmd_line, l)
+    end
+  end
+  vim.api.nvim_echo(cmd_line, false, {})
+end
+
+function Layout:layout(win)
+  local window_width = vim.api.nvim_win_get_width(win)
+  local width = window_width
+  width = width - self.options.window.padding[2] - self.options.window.padding[4]
+
+  local max_key_width = self:max_width("key")
+  local max_label_width = self:max_width("label")
+  local max_value_width = self:max_width("value")
+
+  local intro_width = max_key_width + 2 + Text.len(self.options.icons.separator) + self.options.layout.spacing
+  local max_width = max_label_width + intro_width + max_value_width
+  if max_width > width then
+    max_width = width
+  end
+
+  local column_width = max_width
+
+  if max_value_width == 0 then
+    if column_width > self.options.layout.width.max then
+      column_width = self.options.layout.width.max
+    end
+    if column_width < self.options.layout.width.min then
+      column_width = self.options.layout.width.min
+    end
+  else
+    max_value_width = math.min(max_value_width, math.floor((column_width - intro_width) / 2))
+  end
+
+  max_label_width = column_width - (intro_width + max_value_width)
+
+  local columns = math.floor(width / column_width)
+
+  local height = math.ceil(#self.items / columns)
+  if height < self.options.layout.height.min then
+    height = self.options.layout.height.min
+  end
+  -- if height > self.options.layout.height.max then height = self.options.layout.height.max end
+
+  local col = 1
+  local row = 1
+  local pad_top = self.options.window.padding[3]
+  local pad_left = self.options.window.padding[4]
+
+  local columns_used = math.min(columns, math.ceil(#self.items / height))
+  local offset_x = 0
+  if columns_used < columns then
+    if self.options.layout.align == "right" then
+      offset_x = (columns - columns_used) * column_width
+    elseif self.options.layout.align == "center" then
+      offset_x = math.floor((columns - columns_used) * column_width / 2)
+    end
+  end
+
+  for _, item in pairs(self.items) do
+    local start = (col - 1) * column_width + self.options.layout.spacing + offset_x
+    if col == 1 then
+      start = start + pad_left
+    end
+    local key = item.key or ""
+    if key == "<lt>" then
+      key = "<"
+    end
+    if key == Util.t("<esc>") then
+      key = "<esc>"
+    end
+    if Text.len(key) < max_key_width then
+      key = string.rep(" ", max_key_width - Text.len(key)) .. key
+    end
+
+    self.text:set(row + pad_top, start, key, "")
+    start = start + Text.len(key) + 1
+
+    self.text:set(row + pad_top, start, self.options.icons.separator, "Separator")
+    start = start + Text.len(self.options.icons.separator) + 1
+
+    if item.value then
+      local value = item.value
+      start = start + 1
+      if Text.len(value) > max_value_width then
+        value = vim.fn.strcharpart(value, 0, max_value_width - 2) .. " …"
+      end
+      self.text:set(row + pad_top, start, value, "Value")
+      if item.highlights then
+        for _, hl in pairs(item.highlights) do
+          self.text:highlight(row + pad_top, start + hl[1] - 1, start + hl[2] - 1, hl[3])
+        end
+      end
+      start = start + max_value_width + 2
+    end
+
+    local label = item.label
+    if Text.len(label) > max_label_width then
+      label = vim.fn.strcharpart(label, 0, max_label_width - 2) .. " …"
+    end
+    self.text:set(row + pad_top, start, label, item.group and "Group" or "Desc")
+
+    if row % height == 0 then
+      col = col + 1
+      row = 1
+    else
+      row = row + 1
+    end
+  end
+
+  for _ = 1, self.options.window.padding[3], 1 do
+    self.text:nl()
+  end
+  self:trail()
+  return self.text
+end
+
+return Layout
