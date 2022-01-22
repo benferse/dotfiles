6ccd9d41b98ff8aec9b0f3e0ef34f5bfb57ed374
Ben Fersenheim <ben@bu.edu>
2022-01-22 00:04:15 -0500

    Evolved better-whitespace config

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -3,18 +3,25 @@
 -- and where we are running
 --
 local g = vim.g
-local map = require('utils').map
+local utils = require('utils')
 
 local function setup()
     --
     -- Vim better whitespace
     --
-    g.better_whitespace_enabled = 2
+    g.better_whitespace_enabled = 0
+    g.strip_only_modified_lines = 1
     g.strip_whitelines_at_eof = 1
+    g.strip_whitespace_confirm = 0
     g.show_spaces_that_precede_tabs = 1
 
-    map('n', ']w', ':NextTrailingWhitespace<cr>')
-    map('n', '[w', ':PrevTrailingWhitespace<cr>')
+    utils.map('n', ']w', ':NextTrailingWhitespace<cr>')
+    utils.map('n', '[w', ':PrevTrailingWhitespace<cr>')
+
+    utils.augroup('benferse#trailing_whitespace', {
+        [[FileType c,cpp,json,lua,rust,toml EnableWhitespace]],
+        [[FileType c,cpp,json,lua,rust,toml EnableStripWhitespaceOnSave]]
+    })
 end
 
 return { setup = setup }
