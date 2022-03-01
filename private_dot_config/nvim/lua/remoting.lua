fff54472ac2ca49ad0ebdf79ddb7e59c72b888b7
Ben Fersenheim <ben@bu.edu>
2022-03-01 12:02:29 -0500

    More consistent handling of EDITOR with nvr

--- private_dot_config/nvim/lua/remoting.lua
+++ private_dot_config/nvim/lua/remoting.lua
@@ -1,16 +1,10 @@
 -- Remote control an instance management of a running
 -- nvim server
 
-if vim.fn.has('win32') == 0 then
+if vim.fn.has('win32') == 0 and vim.fn.executable('nvr') then
     -- Make sure that internal terminals can launch this
     -- server as git's preferred editor
-    vim.fn.setenv('GIT_EDITOR', [[nvr -cc split --remote-wait]])
-
-    -- Delete temporary git message buffers as soon as possible
-    -- so git stops waiting for us
-    vim.cmd [[
-        augroup git_autoclose
-            autocmd! FileType gitcommit,gitrebase,gitconfig set bufhidden=delete
-        augroup END
-    ]]
+    vim.fn.setenv('GIT_EDITOR', [[nvr -s --remote-tab-wait +'set bufhidden=wipe']])
+    vim.fn.setenv('EDITOR', [[nvr -s --remote-tab-wait +'set bufhidden=wipe']])
+    vim.fn.setenv('VISUAL', [[nvr -s --remote-tab-wait +'set bufhidden=wipe']])
 end
