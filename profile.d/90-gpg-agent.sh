c9ed9fa3c5e68ba29eb87b81fe57c474219f23da
Ben Fersenheim <benferse@microsoft.com>
2022-05-03 14:26:39 -0400

    Updates to picom and dunst

--- profile.d/90-gpg-agent.sh
+++ profile.d/90-gpg-agent.sh
@@ -1,4 +1,5 @@
 # always favor the gpg-agent's ssh socket
-if (command -v gpgconf >/dev/null 2>&1); then
-    export SSH_AUTH_SOCK=`gpgconf --list-dirs agent-ssh-socket`
+ssh_sock="/run/user/$(id -u)/gnupg/S.gpg-agent.ssh"
+if [ -S $ssh_sock ]; then
+    export SSH_AUTH_SOCK=$ssh_sock
 fi
