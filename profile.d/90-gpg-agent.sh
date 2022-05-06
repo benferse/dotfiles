7239fe544a1501f2207c2bf6afa565049089c39f
Ben Fersenheim <benferse@microsoft.com>
2022-05-06 15:09:17 -0400

    Updates to shell profile and kitty

--- profile.d/90-gpg-agent.sh
+++ profile.d/90-gpg-agent.sh
@@ -1,5 +1,2 @@
 # always favor the gpg-agent's ssh socket
-ssh_sock="/run/user/$(id -u)/gnupg/S.gpg-agent.ssh"
-if [ -S $ssh_sock ]; then
-    export SSH_AUTH_SOCK=$ssh_sock
-fi
+export SSH_AUTH_SOCK="/run/user/$(id -u)/gnupg/S.gpg-agent.ssh"
