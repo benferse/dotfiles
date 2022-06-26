a2b05bc3f4650b6409bf45ce05519588f8d6874f
Ben Fersenheim <benferse@microsoft.com>
2022-06-26 15:01:57 -0400

    systemd unit overrides for gpg-agent with a custom GNUPGHOME

--- profile.d/90-gpg-agent.sh
+++ profile.d/90-gpg-agent.sh
@@ -1,2 +1,7 @@
-# always favor the gpg-agent's ssh socket
-export SSH_AUTH_SOCK="/run/user/$(id -u)/gnupg/S.gpg-agent.ssh"
+# Always favor the gpg-agent's ssh socket, and make sure
+# to use gpgconf(1) to find it. If you set GNUPGHOME, gpg
+# components will add an extra subdir to the socket path
+# (specifically, first 10 bytes of the hash of the full
+# value of GNUPGHOME, encoded with the Zooko variant of
+# base32, because why wouldn't they, right?).
+export SSH_AUTH_SOCK=`gpgconf --list-dirs agent-ssh-socket`
