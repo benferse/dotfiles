e5bded4593f20f0e59aa10d7d2a502928ec86a3b
Ben Fersenheim <benferse@microsoft.com>
2022-06-27 12:45:32 -0400

    Add nvim-treesitter-textsubjects

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -166,6 +166,7 @@ return packer.startup(function(use)
     --
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use { 'nvim-treesitter/nvim-treesitter-textobjects' }
+    use { 'rrethy/nvim-treesitter-textsubjects' }
 
     --
     -- Language Server Protocol support, as well as support for
