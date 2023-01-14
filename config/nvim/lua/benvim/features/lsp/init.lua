60f8cd52f78d80605903d92fc6e90e8f65dbf9f8
Ben Fersenheim <benferse@microsoft.com>
2023-01-13 19:38:09 -0500

    Diagnostic signs and init todo

--- config/nvim/lua/benvim/features/lsp/init.lua
+++ config/nvim/lua/benvim/features/lsp/init.lua
@@ -47,6 +47,11 @@ return {
                     require("lspconfig")[server].setup(opts)
                 end,
             })
+
+            vim.fn.sign_define("DiagnosticSignError", { texthl = "DiagnosticSignError", text = "", numhl = "" })
+            vim.fn.sign_define("DiagnosticSignWarn", { texthl = "DiagnosticSignWarn", text = "", numhl = "" })
+            vim.fn.sign_define("DiagnosticSignInfo", { texthl = "DiagnosticSignInfo", text = "", numhl = "" })
+            vim.fn.sign_define("DiagnosticSignHint", { texthl = "DiagnosticSignHint", text = "", numhl = "" })
         end,
     },
     {
