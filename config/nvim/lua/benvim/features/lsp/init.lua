acc42c9808c010bea73819a57062e714edc5f494
Ben Fersenheim <benferse@microsoft.com>
2023-01-26 19:31:21 -0500

    wip

--- config/nvim/lua/benvim/features/lsp/init.lua
+++ config/nvim/lua/benvim/features/lsp/init.lua
@@ -9,40 +9,71 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "smiteshp/nvim-navic",
         },
-        servers = {
-            clangd = {},
-            rust_analyzer = require("benvim.features.lsp.rust_analyzer.settings"),
-            sumneko_lua = require("benvim.features.lsp.sumneko_lua.settings"),
+        opts = {
+            diagnostics = {
+                underline = true,
+                update_in_insert = true,
+                virtual_text = {
+                    spacing = 4,
+                    prefix = "●",
+                },
+                severity_sort = true,
+            },
+            servers = {
+                clangd = {},
+                rust_analyzer = require("benvim.features.lsp.rust_analyzer.settings"),
+                sumneko_lua = require("benvim.features.lsp.sumneko_lua.settings"),
+            },
         },
-        config = function(plugin)
+        config = function(_, opts)
             require("neodev").setup()
 
+            -- Setup a hook to run when an LSP client attaches to a buffer.
+            -- We'll do any LSP-specific setup then
             vim.api.nvim_create_autocmd("LspAttach", {
                 callback = function(args)
                     local bufnr = args.buf
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+                    -- Load any LSP-specific keymaps
                     require("benvim.features.lsp.keymaps").on_attach(client, bufnr)
 
+                    -- Set LSP api options to connect standard vim features to LSP capabilities:
+                    -- - connect formatting to the "formatexpr" expression
+                    -- - connect omni completion to the LSP client's completion
+                    -- - connect tag searching (e.g. CTRL-]) to the "textDocument/definition" or
+                    --   "workspace/symbol" capabilities
+                    --
+                    -- These are all also exposed from LSP-specific workflows, but this way anything
+                    -- that is trying to use one of the vim entry points will work the same way
                     vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
                     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
                     vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.vim.lsp.tagfunc")
 
+                    -- If the client that attached supports doc symbol providing, then attach
+                    -- navic to this buffer as well
                     if client.server_capabilities.documentSymbolProvider then
                         require("nvim-navic").attach(client, bufnr)
                     end
                 end,
             })
 
+            -- Mason will take care of LSP setup; all we need to do it make any tweaks to
+            -- capabilities that we'd like to see
             local default_caps = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-            require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(plugin.servers) })
+            require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(opts.servers) })
             require("mason-lspconfig").setup_handlers({
                 function(server)
-                    local opts = plugin.servers[server] or {}
-                    opts.capabilities = default_caps
-                    require("lspconfig")[server].setup(opts)
+                    local server_opts = opts.servers[server] or {}
+                    server_opts.capabilities = default_caps
+                    require("lspconfig")[server].setup(server_opts)
                 end,
             })
 
+            -- Our diagnostic configuration is (currently) global, so it can be set once and done
+            vim.diagnostic.config(opts.diagnostics)
+
+            -- Set up our diagnostic signs the way that we like
             vim.fn.sign_define("DiagnosticSignError", { texthl = "DiagnosticSignError", text = "", numhl = "" })
             vim.fn.sign_define("DiagnosticSignWarn", { texthl = "DiagnosticSignWarn", text = "", numhl = "" })
             vim.fn.sign_define("DiagnosticSignInfo", { texthl = "DiagnosticSignInfo", text = "", numhl = "" })
@@ -55,19 +86,22 @@ return {
         keys = {
             { "<leader>um", "<cmd>Mason<cr>", desc = "LSPs/Formatters (Mason)" },
         },
-        config = function()
-            require("mason").setup({})
-            local reg = require("mason-registry")
-            local ensure_installed = {
+        opts = {
+            ensure_installed = {
                 "clangd",
+                "codelldb",
                 "lua-language-server",
                 "rust-analyzer",
                 "rustfmt",
                 "shellcheck",
                 "shfmt",
                 "stylua",
-            }
-            for _, tool in ipairs(ensure_installed) do
+            },
+        },
+        config = function(_, opts)
+            require("mason").setup(opts)
+            local reg = require("mason-registry")
+            for _, tool in ipairs(opts.ensure_installed) do
                 local p = reg.get_package(tool)
                 if not p:is_installed() then
                     p:install()
@@ -78,6 +112,8 @@ return {
     {
         "simrat39/rust-tools.nvim",
         dependencies = {
+            "williamboman/mason.nvim",
+            "mfussenegger/nvim-dap",
             "neovim/nvim-lspconfig",
         },
         ft = "rust",
@@ -87,6 +123,18 @@ return {
                     auto = true,
                 },
             },
+            dap = {
+                adapter = {},
+            },
         },
+        config = function(_, opts)
+            local mason_path = vim.fn.stdpath("data") .. "/mason/"
+            local codelldb_path = mason_path .. "bin/codelldb"
+            local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb.so"
+
+            opts.dap.adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+
+            require("rust-tools").setup(opts)
+        end,
     },
 }
