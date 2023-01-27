e5eb7486c35b612f8ac5ba91bd82ee6c05d0b7ff
Ben Fersenheim <benferse@microsoft.com>
2023-01-26 20:52:21 -0500

    more wip

--- config/nvim/lua/benvim/features/lsp/rust_analyzer/keymaps.lua
+++ config/nvim/lua/benvim/features/lsp/rust_analyzer/keymaps.lua
@@ -1,6 +1,9 @@
 local M = {}
 
 function M.on_attach(client, bufnr)
+    -- rust-tools supports injecting additional actions into the hover window,
+    -- so replace the standard hover keymap with the Rust one
+    vim.keymap.set("n", "K", "<cmd>RustHoverActions<cr>", { buffer = bufnr })
 end
 
 return M
