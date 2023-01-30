75a97da6c3200211d914028f9eed4cfa1ab3b603
Ben Fersenheim <benferse@microsoft.com>
2023-01-30 17:35:39 -0500

    Fix luacheck warnings

--- config/nvim/lua/benvim/features/completion.lua
+++ config/nvim/lua/benvim/features/completion.lua
@@ -1,4 +1,6 @@
 local has_words_before = function()
+    -- `table.unpack` is introduced in Lua 5.2/5.3/5.4, but neovim's LuaJIT doesn't grok it
+    ---@diagnostic disable-next-line deprecated
     unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
