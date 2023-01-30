04094d4ddedc33c67569f3e8b4e7b5d2fa61e0d1
Ben Fersenheim <benferse@microsoft.com>
2023-01-30 17:35:39 -0500

    Fix luacheck warnings

--- config/nvim/lua/benvim/features/interface.lua
+++ config/nvim/lua/benvim/features/interface.lua
@@ -121,10 +121,15 @@ return {
     {
         "stevearc/dressing.nvim",
         init = function()
+            -- We are setting this twice, but that is indeed the point
+            ---@diagnostic disable-next-line duplicate-set-field
             vim.ui.select = function(...)
                 require("lazy").load({ plugins = { "dressing.nvim" }})
                 return vim.ui.select(...)
             end
+
+            -- We are setting this twice, but that is indeed the point
+            ---@diagnostic disable-next-line duplicate-set-field
             vim.ui.input = function(...)
                 require("lazy").load({ plugins = { "dressing.nvim" }})
                 return vim.ui.input(...)
--- config/nvim/lua/benvim/features/lsp/sumneko_lua/settings.lua
+++ config/nvim/lua/benvim/features/lsp/sumneko_lua/settings.lua
@@ -1,11 +1,17 @@
 return {
     settings = {
         Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
             workspace = {
                 checkThirdParty = false,
             },
             completion = {
                 callSnippet = "Replace",
+            },
+            telemetry = {
+                enable = false,
             },
         },
     },
