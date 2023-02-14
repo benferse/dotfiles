e1c1b03bfdea6d90f6014d377d48f1bdbc4babad
Ben Fersenheim <benferse@microsoft.com>
2023-02-14 17:13:37 -0500

    Switch back to comment.nvim

--- config/nvim/lua/benvim/features/treesitter.lua
+++ config/nvim/lua/benvim/features/treesitter.lua
@@ -3,9 +3,6 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        },
         build = ":TSUpdate",
         event = "BufReadPost",
         config = function()
