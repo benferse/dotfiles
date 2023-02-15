045c94d595c19b80a89c55e06e0003d32f23811d
Ben Fersenheim <benferse@microsoft.com>
2023-02-14 21:30:53 -0500

    No auto-completion suggestions while inside a comment

--- config/nvim/lua/benvim/features/completion.lua
+++ config/nvim/lua/benvim/features/completion.lua
@@ -48,6 +48,17 @@ return {
             local lspkind = require("lspkind")
 
             cmp.setup({
+                enabled = function()
+                    -- Disable auto-completion when inside a comment (but leave command mode
+                    -- completion enabled)
+                    local context = require("cmp.config.context")
+                    if vim.api.nvim_get_mode().mode == "c" then
+                        return true
+                    else
+                        return not context.in_treesitter_capture("comment")
+                            and not context.in_syntax_group("Comment")
+                    end
+                end,
                 completion = {
                     completeopt = "menu,menuone,noinsert",
                 },
