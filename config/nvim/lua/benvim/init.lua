640689fb271b3ed32429462bbdd03da1699555f0
Ben Fersenheim <benferse@microsoft.com>
2023-02-15 19:20:19 -0500

    Add is_in_ts_capture utility function

--- config/nvim/lua/benvim/init.lua
+++ config/nvim/lua/benvim/init.lua
@@ -2,6 +2,38 @@ local M = {}
 
 M.root_patterns = { ".git" }
 
+-- Check whether the current position is inside a given treesitter capture context.
+-- Note that this returns the leaf (most specific context), so it's best used for
+-- contexts that don't contain other contexts
+---@param capture string | []string
+---@return boolean
+function M.is_in_ts_capture(capture)
+    local buf = vim.api.nvim_get_current_buf()
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    row = row - 1
+
+    if vim.api.nvim_get_mode().mode == "i" then
+        col = col - 1
+    end
+
+    local get_captures_at_pos = require("vim.treesitter").get_captures_at_pos
+    local captures = vim.tbl_map(function(x) return x.capture end, get_captures_at_pos(buf, row, col))
+
+    if vim.tbl_isempty(captures) then
+        return false
+    elseif type(capture) == "string" and vim.tbl_contains(captures, capture) then
+        return true
+    elseif type(capture) == "table" then
+        for _, v in ipairs(capture) do
+            if vim.tbl_contains(captures, v) then
+                return true
+            end
+        end
+    end
+
+    return false
+end
+
 -- Determine the "root" directory, using several potential heuristics, such as LSP folders,
 -- git information, or arbitrary patterns. Shout out to folke, see
 -- https://github.com/folke/LazyVim/blob/main/lua/lazyvim/util.lua
