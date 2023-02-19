f0cce4b0803e74c7d90b7ca9fa8e6f4889c95174
Ben Fersenheim <benferse@microsoft.com>
2023-02-19 14:01:59 -0500

    Delay all notifications until noice and nvim-notify finish loading

--- config/nvim/lua/benvim/boot.lua
+++ config/nvim/lua/benvim/boot.lua
@@ -19,6 +19,51 @@ vim.opt.runtimepath:prepend(plugin_mgr)
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
+-- Delay notifications until someone replaces vim.notify (which should happen if our
+-- plugins load correctly), or after a second (in which case something is probably very
+-- very wrong)
+local deferred_notifications = {}
+local fallback_timer = vim.loop.new_timer()
+local check_replacement = vim.loop.new_check()
+
+if fallback_timer and check_replacement then
+    -- Temporary replacement for vim.notify that will just stash anything
+    -- it receives
+    local function defer(...)
+        table.insert(deferred_notifications, vim.F.pack_len(...))
+    end
+
+    local original_notify = vim.notify
+    vim.notify = defer
+
+    local replay_deferred = function()
+        -- Stop both background tasks
+        fallback_timer:stop()
+        check_replacement:stop()
+
+        -- Restore vim.notify if necessary
+        if vim.notify == defer then
+            vim.notify = original_notify
+        end
+
+        vim.schedule(function()
+            for _, notification in ipairs(deferred_notifications) do
+                vim.notify(vim.F.unpack_len(notification))
+            end
+        end)
+    end
+
+    -- Start the task that waits for vim.notify to be replaced
+    check_replacement:start(function()
+        if vim.notify ~= original_notify then
+            replay_deferred()
+        end
+    end)
+
+    -- Start the timer that will stop waiting after a second
+    fallback_timer:start(1000, 0, replay_deferred)
+end
+
 -- Direct the plugin manager to the spec that contains our plugins
 -- and their config. Plugins are collected into logic "features" to
 -- keep related things together and prevent a lot of overhead due to
@@ -30,7 +75,7 @@ require("lazy").setup({
         version = "*",
     },
     install = {
-        colorscheme = { "nord", "habamax", },
+        colorscheme = { "nord", "habamax" },
     },
     checker = {
         enabled = true,
@@ -54,4 +99,4 @@ require("lazy").setup({
     },
 })
 
-vim.keymap.set("n", "<leader>ul", "<cmd>:Lazy<cr>", { desc = "Plugins (Lazy)"})
+vim.keymap.set("n", "<leader>ul", "<cmd>:Lazy<cr>", { desc = "Plugins (Lazy)" })
