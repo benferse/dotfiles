a5f546442538e3ca2d7c95db9ce332d29c6d9cbb
Ben Fersenheim <ben@bu.edu>
2023-10-29 22:36:35 -0400

    Adapt health checks for 0.9 or 0.10+

--- config/nvim/lua/benvim/health.lua
+++ config/nvim/lua/benvim/health.lua
@@ -1,11 +1,24 @@
 local M = {}
 
+-- Avoid deprecation warnings for older health check functions if we're not running
+-- 0.10 or higher
+local v10 = vim.fn.has("nvim-0.10.0")
+
+local function start(name) if v10 then vim.health.start(name) else vim.health.report_start(name) end end
+local function ok(msg) if v10 then vim.health.ok(msg) else vim.health.report_ok(msg) end end
+local function info(msg) if v10 then vim.health.info(msg) else vim.health.report_info(msg) end end
+local function warn(msg) if v10 then vim.health.warn(msg) else vim.health.report_warn(msg) end end
+local function error(msg) if v10 then vim.health.error(msg) else vim.health.report_error(msg) end end
+
 function M.check()
-    vim.health.report_start("benvim")
-    if vim.fn.has("nvim-0.9.0") then
-        vim.health.report_ok("Using Neovim >= 0.9.0")
+    start("benvim")
+
+    if vim.fn.has("nvim-0.10.0") then
+        ok("Using Neovim >= 0.10.0 (best experience)")
+    elseif vim.fn.has("nvim-0.9.0") then
+        info("Using Neovim >= 0.9.0 works but >= 0.10.0 is better")
     else
-        vim.health.report_warn("This has only been tested with Neovim 0.8 and above!")
+        warn("This has only been tested with Neovim 0.9 and above!")
     end
 
     for _, cmd in ipairs({ "git", "rg", "fd", "lazygit", "btop" }) do
@@ -14,12 +27,12 @@ function M.check()
             if h ~= nil then
                 local v = h:read("*l")
                 h:close()
-                vim.health.report_ok(("`%s --version` returns: %s"):format(cmd, v))
+                ok(("`%s --version` returns: %s"):format(cmd, v))
             else
-                vim.health.report_error("Couldn't run " .. cmd)
+                error("Couldn't run " .. cmd)
             end
         else
-            vim.health.report_warn(("`%s` is not installed"):format(cmd))
+            warn(("`%s` is not installed"):format(cmd))
         end
     end
 end
