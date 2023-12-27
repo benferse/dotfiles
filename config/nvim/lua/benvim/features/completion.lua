5ada71c800e722e4f79877c0670c86aa7e056c6b
Ben Fersenheim <benferse@microsoft.com>
2023-12-27 09:55:44 -0500

    Plugin updates

--- config/nvim/lua/benvim/features/completion.lua
+++ config/nvim/lua/benvim/features/completion.lua
@@ -103,7 +103,6 @@ return {
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
                     { name = "luasnip" },
-                    { name = "buffer" },
                     { name = "path" },
                     { name = "emoji" },
                 }),
