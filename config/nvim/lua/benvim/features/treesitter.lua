ac8d09f90b4dc1aeb52720d3f0ac64a81ad67055
Ben Fersenheim <benferse@microsoft.com>
2024-01-12 11:29:13 -0500

    Disable a verbose warning from lua_ls on perfectly reasonable config

--- config/nvim/lua/benvim/features/treesitter.lua
+++ config/nvim/lua/benvim/features/treesitter.lua
@@ -7,6 +7,8 @@ return {
         build = ":TSUpdate",
         event = "BufReadPost",
         config = function()
+            -- lua_ls is a little to overzealous here
+            ---@diagnostic disable-next-line: missing-fields
             require("nvim-treesitter.configs").setup({
                 sync_install = false,
                 ensure_installed = {
