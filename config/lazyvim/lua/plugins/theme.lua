53fe428250bbe08214e556f55e9fbcf060864ed3
Ben Fersenheim <benferse@microsoft.com>
2024-02-07 09:58:09 -0500

    No giant LAZYVIM pls

--- config/lazyvim/lua/plugins/theme.lua
+++ config/lazyvim/lua/plugins/theme.lua
@@ -20,4 +20,18 @@ return {
       colorscheme = "nord",
     },
   },
+
+  -- We don't need the giant logo on the splash screen
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function()
+      local v = vim.version() or { major = 0, minor = 0, patch = 0 }
+      local banner = "benvim v2024 / neovim " .. v.major .. "." .. v.minor .. "." .. v.patch
+      return {
+        config = {
+          header = { banner },
+        },
+      }
+    end,
+  },
 }
