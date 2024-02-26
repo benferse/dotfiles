4c34fb502cc89ea6593672d738aab4b73b2221f2
Ben <benferse@users.noreply.github.com>
2024-02-26 13:09:34 -0500

    Promote lazyvim to main (#9)
    * Add lazyvim
    
    * No giant LAZYVIM pls
    
    * Fix M-CR keymap for macos
    
    * Enable aerial.nvim
    
    * Enable vscode extra
    
    * Plugin updates
    
    * Disable nvim-cmp inside comment-like-things
    
    * Plugin updates
    
    * Promote lazyvim config to default
    
    * Replace various <leader>w bindings
    
    * Plugin updates
    
    * Knock knock. Who's there? Plugin updates
    
    * Activate dap extras for lazyvim
    
    * Use beyond compare as the interactive diff/merge tool
    
    * Add some vs/windbg style keybinds for debuggee control
    
    * Plugin updates
    
    * Enable the YAML language extras
    
    * Plugin updates
    
    * Plugin updates

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,72 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Keymaps in this file are non plugin specific, and will use basic vim/nvim features or
+-- our own functions. Well, built-in features. No promise on whether or not they're basic :)
+
+-- Escape back to normal mode without having to stretch
+vim.keymap.set("i", "kj", "<cmd>noh<cr><esc>")
+vim.keymap.set("i", "jj", "<cmd>noh<cr><esc>")
+
+-- Undo breakpoints for code editing. Insert logical places to "undo" to within a single
+-- line
+vim.keymap.set("i", ",", ",<C-g>u")
+vim.keymap.set("i", ".", ".<C-g>u")
+vim.keymap.set("i", ";", ";<C-g>u")
+
+-- This is honestly life changing. I can't believe I lived
+-- for over forty years without this
+vim.keymap.set("n", ";", "<cmd>silent up<cr>")
+
+-- While already in insert mode, move to the end of the next line and continue editing,
+-- or open a new line and continue editing, both of which come up more often than you'd think
+vim.keymap.set("i", "<M-CR>", "<C-O>2g_<C-O>A")
+vim.keymap.set("i", "<S-CR>", "<cmd>normal! O<cr>")
+vim.keymap.set("i", "<C-CR>", "<cmd>normal! o<cr>")
+
+-- Move lines up or down
+vim.keymap.set("n", "<M-j>", ":m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("v", "<M-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("i", "<M-j>", "<Esc>:m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("n", "<M-k>", ":m .-2<cr>==", { desc = "Move down" })
+vim.keymap.set("v", "<M-k>", ":m '<-2<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("i", "<M-k>", "<Esc>:m .-2<cr>==gi", { desc = "Move down" })
+
+-- Mappings that start with [ or ] generally come in paris and involve next/prev or fwd/back
+-- navigations that come in pairs, a-la vim-unimpaired
+vim.keymap.set({ "n", "v" }, "]<Tab>", "gt", { desc = "Tab page" })
+vim.keymap.set({ "n", "v" }, "[<Tab>", "gT", { desc = "Tab page" })
+
+-- Additional mappings for <leader>w for window management, keeping with the overall
+-- pattern that <leader>wX is equivalent to <C-W>X
+vim.keymap.set("n", "<leader>wh", "<C-W>h", { desc = "Left window" })
+vim.keymap.set("n", "<leader>wj", "<C-W>j", { desc = "Lower window" })
+vim.keymap.set("n", "<leader>wk", "<C-W>k", { desc = "Upper window" })
+vim.keymap.set("n", "<leader>wl", "<C-W>l", { desc = "Right window" })
+vim.keymap.set("n", "<leader>wo", "<C-W>o", { desc = "Only this window" })
+
+-- ]c and [c move between diff chunks in diffmode, while ]h and [h move between git hunks in
+-- non-diff mode. Those are close enough operations; wouldn't it be nice if they were consistent?
+-- I agree.
+vim.keymap.set("n", "]h", [[&diff ? ']c' : ']h']], { expr = true, desc = "Difference", remap = true })
+vim.keymap.set("n", "[h", [[&diff ? '[c' : '[h']], { expr = true, desc = "Difference", remap = true })
+
+vim.keymap.set("n", "]Q", "<cmd>clast<cr>", { desc = "Last trouble/quickfix item" })
+vim.keymap.set("n", "[Q", "<cmd>cfirst<cr>", { desc = "First trouble/quickfix item" })
+
+vim.keymap.set("n", "[y", "<cmd>AerialPrev<cr>", { desc = "Symbol (Aerial)" })
+vim.keymap.set("n", "]y", "<cmd>AerialNext<cr>", { desc = "Symbol (Aerial)" })
+
+-- Custom textobjects for the entire document (mnemonic "an everything" :))
+local function entire_buffer_textobj()
+  local mode = vim.api.nvim_get_mode().mode
+  local is_visual = string.lower(string.sub(mode, 1, 1)) == "v"
+
+  vim.fn.execute("normal! m`")
+  vim.fn.execute("keepjumps normal! gg0")
+  vim.fn.execute("normal! " .. (is_visual and "o" or "V"))
+  vim.fn.execute("keepjumps normal! G$")
+end
+
+vim.keymap.set({ "o", "x" }, "ae", entire_buffer_textobj, { desc = "Entire buffer" })
---
+++ config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,67 @@
+-- Check whether the current position is inside a given syntax highlight group
+local function is_in_syntax_group(group)
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  if vim.api.nvim_get_mode().mode == "i" then
+    col = col - 1
+  end
+
+  for _, syn_id in ipairs(vim.fn.synstack(row, col)) do
+    syn_id = vim.fn.synIDtrans(syn_id)
+    local g = vim.fn.synIDattr(syn_id, "name")
+    if type(group) == "string" and g == group then
+      return true
+    elseif type(group) == "table" and vim.tbl_contains(group, g) then
+      return true
+    end
+  end
+
+  return false
+end
+
+-- Check whether the current position is inside a given treesitter capture context.
+-- Note that this returns the leaf (most specific context), so it's best used for
+-- contexts that don't contain other contexts
+---@param capture string | []string
+---@return boolean
+local function is_in_ts_capture(capture)
+  local buf = vim.api.nvim_get_current_buf()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  row = row - 1
+
+  if vim.api.nvim_get_mode().mode == "i" then
+    col = col - 1
+  end
+
+  local get_captures_at_pos = require("vim.treesitter").get_captures_at_pos
+  local captures = vim.tbl_map(function(x)
+    return x.capture
+  end, get_captures_at_pos(buf, row, col))
+
+  if vim.tbl_isempty(captures) then
+    return false
+  elseif type(capture) == "string" and vim.tbl_contains(captures, capture) then
+    return true
+  elseif type(capture) == "table" then
+    for _, v in ipairs(capture) do
+      if vim.tbl_contains(captures, v) then
+        return true
+      end
+    end
+  end
+
+  return false
+end
+
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    opts.enabled = function()
+      -- Disable autocompletion when inside a comment (but leave command mode completion enabled)
+      if vim.api.nvim_get_mode().mode == "c" then
+        return true
+      else
+        return not is_in_ts_capture("comment") and not is_in_syntax_group("Comment")
+      end
+    end
+  end,
+}
