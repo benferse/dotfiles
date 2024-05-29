9a96bd7e359c3514e206311ddb7fa31fc24ec996
Ben Fersenheim <benferse@microsoft.com>
2024-05-29 14:29:09 -0400

    Neovim 0.10 updates so far

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -53,15 +53,58 @@ local function is_in_ts_capture(capture)
 end
 
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    opts.enabled = function()
-      -- Disable autocompletion when inside a comment (but leave command mode completion enabled)
-      if vim.api.nvim_get_mode().mode == "c" then
-        return true
-      else
-        return not is_in_ts_capture("comment") and not is_in_syntax_group("Comment")
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
-    end
-  end,
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.select_next_item()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+
+      opts.enabled = function()
+        -- Disable autocompletion when inside a comment (but leave command mode completion enabled)
+        if vim.api.nvim_get_mode().mode == "c" then
+          return true
+        else
+          return not is_in_ts_capture("comment") and not is_in_syntax_group("Comment")
+        end
+      end
+    end,
+  },
 }
